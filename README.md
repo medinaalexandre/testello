88dfae69d4299e733fc98047447fd4e547d8de69
Medina <ammedina@inf.ufsm.br>
2023-02-05 11:05:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Testello
+--
+### Ambiente
+Com o intuito de agilizar a preparação do ambiente de desenvolvimento com docker, foi utilizado o pacote [Laravel Sail](https://laravel.com/docs/9.x/sail).
+Para subir os containers, execute o seguinte comando na pasta raiz do projeto
+```shell
+./vendor/bin/sail up
+```
---
+++ database/migrations/2023_02_05_135215_create_customers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id('customer_id');
+            $table->string('name')->unique();
+            $table->string('email')->nullable()->unique();
+            $table->dateTime('last_readjustment_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
